8e1739cbf7b3290042feae61c45f6b8b1d6b4bec
taismichely <michely2207@gmail.com>
2025-07-08 16:46:19 -0300

    feat: Adicionando CategoriaService e CategoriaController na CategoriaModule

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from './entities/categoria.entity';
+import { CategoriaService } from './services/categoria.service';
+import { CategoriaController } from './controllers/categoria.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Categoria])],
-  providers: [],
+  providers: [CategoriaService],
   exports: [],
-  controllers: [],
+  controllers: [CategoriaController],
 })
 export class CategoriaModule {}
